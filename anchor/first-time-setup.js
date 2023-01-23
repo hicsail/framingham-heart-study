d6b565f7892e97f7a5ca8797b91be3d527bd870a
wenhwang97 <wenhwang@bu.edu>
2023-01-23 13:32:40 -0500

    feat: add populate proposals to setup script

--- anchor/first-time-setup.js
+++ anchor/first-time-setup.js
@@ -1,54 +1,63 @@
-'use strict';
-const Config = require('./config');
-const Joi = require('joi');
-const AnchorModels = require('./server/anchor/anchor-model');
-const Mongodb = require('mongodb');
-const Promptly = require('promptly');
-const User = require('./server/models/user');
-const PasswordComplexity = require('joi-password-complexity');
- 
-
+"use strict";
+const Config = require("./config");
+const Joi = require("joi");
+const AnchorModels = require("./server/anchor/anchor-model");
+const Mongodb = require("mongodb");
+const Promptly = require("promptly");
+const User = require("./server/models/user");
+const PasswordComplexity = require("joi-password-complexity");
+const Proposal = require("./server/models/proposal");
 
 const main = async function () {
-
-  const hapiAnchorModel = Config.get('/hapiAnchorModel');
-  const defualtMongoURI = hapiAnchorModel.mongodb.connection.uri + hapiAnchorModel.mongodb.connection.db;
+  const hapiAnchorModel = Config.get("/hapiAnchorModel");
+  const defualtMongoURI =
+    hapiAnchorModel.mongodb.connection.uri +
+    hapiAnchorModel.mongodb.connection.db;
   const options = {
-    default: defualtMongoURI
-  };  
-  
-  const mongodbUri = await Promptly.prompt(`MongoDB URI: (${defualtMongoURI})`, options);
-  const testMongo = await Mongodb.MongoClient.connect(mongodbUri, {}); 
+    default: defualtMongoURI,
+  };
+
+  const mongodbUri = await Promptly.prompt(
+    `MongoDB URI: (${defualtMongoURI})`,
+    options
+  );
+  const testMongo = await Mongodb.MongoClient.connect(mongodbUri, {});
 
   if (!testMongo) {
-    console.error('Failed to connect to Mongodb.');
+    console.error("Failed to connect to Mongodb.");
   }
 
-  const rootEmail = await Promptly.prompt('Root user email:');
-  const rootPassword = await Promptly.password('Root user password:');
-  const complexityOptions = Config.get('/passwordComplexity');
+  const rootEmail = await Promptly.prompt("Root user email:");
+  const rootPassword = await Promptly.password("Root user password:");
+  const complexityOptions = Config.get("/passwordComplexity");
   Joi.validate(rootPassword, new PasswordComplexity(complexityOptions));
-  
-  const connection = { 'uri': mongodbUri ,'db': `${hapiAnchorModel.mongodb.connection.db}`};
+
+  const connection = {
+    uri: mongodbUri,
+    db: `${hapiAnchorModel.mongodb.connection.db}`,
+  };
   await AnchorModels.connect(connection, {});
-  
-  const userEmail = await User.findOne({ email: rootEmail });  
+
+  const userEmail = await User.findOne({ email: rootEmail });
   if (userEmail) {
-    console.err(Error('Email is in use'));
+    console.err(Error("Email is in use"));
   }
   const passwordHash = await User.generatePasswordHash(rootPassword);
   const document = {
-    _id: User.ObjectId('000000000000000000000000'),     
-    name: 'Root',
+    _id: User.ObjectId("000000000000000000000000"),
+    name: "Root",
     password: passwordHash.hash,
     email: rootEmail.toLowerCase(),
     roles: {
-      root: true
+      root: true,
     },
-    timeCreated: new Date()
+    timeCreated: new Date(),
   };
   await User.insertOne(document);
-  console.log('Setup complete.');
-  process.exit(0);  
+
+  await Proposal.populate();
+
+  console.log("Setup complete.");
+  process.exit(0);
 };
 main();
