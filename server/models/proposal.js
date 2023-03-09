d05d4c16a0d2a92b37948f53436c85f524a88bfa
wenhwang97 <wenhwang@bu.edu>
2023-03-09 11:05:42 -0500

    refactor: reformat the code

--- server/models/proposal.js
+++ server/models/proposal.js
@@ -2,38 +2,35 @@
 const Joi = require("joi");
 const AnchorModel = require("../anchor/anchor-model");
 const Hoek = require("hoek");
-const Assert = require('assert');
+const Assert = require("assert");
 
-class Proposal extends AnchorModel { 
-
+class Proposal extends AnchorModel {
   static async create(doc) {
+    Assert.ok(doc.userId, "Missing userId argument.");
+    Assert.ok(doc.fileName, "Missing file name argument.");
 
-    Assert.ok(doc.userId, 'Missing userId argument.');
-    Assert.ok(doc.fileName, 'Missing file name argument.');
-
-    const document = new this({  
-      fileName: doc.fileName,    
+    const document = new this({
+      fileName: doc.fileName,
       userId: doc.userId, //userId of the person who uploads the doc
-      groupId: doc.groupId ? doc.groupId : null, // we link proposals (revised ones) using groupId      
-      reviewerIds: [], // list of assigned reviwers 
-      feasibilityStatus: this.status.PENDING,           
+      groupId: doc.groupId ? doc.groupId : null, // we link proposals (revised ones) using groupId
+      reviewerIds: [], // list of assigned reviwers
+      feasibilityStatus: this.status.PENDING,
       feasibilityReviewDate: null,
-      feasibilityReviewerId: null      
+      feasibilityReviewerId: null,
     });
     return this.insertOne(document);
   }
 
-  static async createMany(docs) { 
-
-    for (const doc of docs)  {
-      Assert.ok(doc.userId, 'Missing userId argument.');
-      Assert.ok(doc.fileName, 'Missing file name argument.');
+  static async createMany(docs) {
+    for (const doc of docs) {
+      Assert.ok(doc.userId, "Missing userId argument.");
+      Assert.ok(doc.fileName, "Missing file name argument.");
 
       doc.reviewerIds = [];
-      doc.feasibilityStatus = this.status.PENDING; 
-      doc.feasibilityReviewerId = null;           
+      doc.feasibilityStatus = this.status.PENDING;
+      doc.feasibilityReviewerId = null;
       doc.feasibilityReviewDate = null;
-      doc.groupId = doc.groupId ? doc.groupId : null;     
+      doc.groupId = doc.groupId ? doc.groupId : null;
     }
 
     const files = await this.insertMany(docs);
@@ -41,23 +38,18 @@ class Proposal extends AnchorModel {
   }
 
   static async findByUploaderId(userId) {
-
     return this.findOne({ userId });
   }
 
-
   static async findManyByFeasibilityStatus(feasibilityStatus) {
-
     return this.find({ feasibilityStatus });
   }
 
   static async findManyByReviewStatus(reviewStatus) {
-
     return this.find({ reviewStatus });
   }
 
   static async updateFeasibilityStatus(docId, userId, feasibilityStatus) {
-
     const update = {
       $set: {
         feasibilityReviewerId: userId,
@@ -70,7 +62,6 @@ class Proposal extends AnchorModel {
   }
 
   static async updateReviewStatus(docId, userId, reviewStatus) {
-
     const update = {
       $set: {
         reviewerId: userId,
@@ -92,30 +83,30 @@ Proposal.status = {
 };
 
 Proposal.schema = Joi.object({
-  _id: Joi.object().required(), 
+  _id: Joi.object().required(),
   groupId: Joi.string().required(),
-  fileName: Joi.string().required(), 
+  fileName: Joi.string().required(),
   userId: Joi.object().required(),
   feasibilityReviewerId: Joi.object().required(),
   reviewerIds: Joi.array().required(),
-  feasibilityStatus: Joi.string().required(),   
+  feasibilityStatus: Joi.string().required(),
   createdAt: Joi.date().required(),
-  feasibilityReviewDate: Joi.date().required()  
+  feasibilityReviewDate: Joi.date().required(),
 });
 
 Proposal.routes = Hoek.applyToDefaults(AnchorModel.routes, {
   create: {
     payload: Joi.object({
       userId: Joi.string().required(),
-      fileName: Joi.string().required()      
+      fileName: Joi.string().required(),
     }),
-  },  
+  },
   insertMany: {
     disabled: false,
     payload: Joi.object({
       userId: Joi.string().required(),
-      fileName: Joi.string().required()           
-    })
+      fileName: Joi.string().required(),
+    }),
   },
 });
 
@@ -127,19 +118,19 @@ Proposal.payload = Joi.object({
 
 Proposal.lookups = [
   {
-    from: require('./user'),
-    local: 'userId',
-    foreign: '_id',
-    as: 'user',
-    one: true
-  },  
+    from: require("./user"),
+    local: "userId",
+    foreign: "_id",
+    as: "user",
+    one: true,
+  },
   {
-    from: require('./user'),
-    local: 'feasibilityReviewerId',
-    foreign: '_id',
-    as: 'feasibilityReviewer',
-    one: true
-  }      
+    from: require("./user"),
+    local: "feasibilityReviewerId",
+    foreign: "_id",
+    as: "feasibilityReviewer",
+    one: true,
+  },
 ];
 
 module.exports = Proposal;
