e63239b96aed326f2379907598bdd474c7d406f8
wenhwang97 <wenhwang@bu.edu>
2023-03-09 15:33:47 -0500

    feat/fix: add feedback decision count and fix view proposals logic for chair

--- server/web/routes/proposals.js
+++ server/web/routes/proposals.js
@@ -9,7 +9,7 @@ const ObjectId = require("mongodb").ObjectID;
 const register = function (server, options) {
   server.route({
     method: "GET",
-    path: "/proposals/review/{proposalId}",
+    path: "/proposals/review/{proposalId}/{reviewerId?}",
     options: {
       auth: {
         strategies: ["session"],
@@ -19,12 +19,41 @@ const register = function (server, options) {
     handler: async function (request, h) {
       const user = request.auth.credentials.user;
       const proposalId = request.params.proposalId;
+      const reviewerId = request.params.reviewerId ? request.params.reviewerId : null;
       const proposal = await Proposal.lookupById(proposalId, Proposal.lookups);
 
-      const feedback = await Feedback.findOne({
-        proposalId: proposalId.toString(),
-        userId: user._id.toString(),
-      });
+      let feedback = null;
+      let reviewers = [];
+
+      if (user.roles.reviewer) {
+        feedback = await Feedback.findOne({
+          proposalId: proposalId.toString(),
+          userId: user._id.toString(),
+        });
+      }
+
+      const resultsDict = {};
+      for (const decision of Object.keys(Feedback.status)) {
+        resultsDict[Feedback.status[decision]] = 0;
+      }
+
+      if (user.roles.chair) {
+        const feedbacks = await Feedback.find({ proposalId: proposalId.toString() });
+        feedback = feedbacks[0];
+        for (let idx = 0; idx < feedbacks.length; idx++) {
+          if (feedbacks[idx].userId === reviewerId) {
+            feedback = feedbacks[idx];
+          }
+
+          resultsDict[feedbacks[idx].decisionTag] += 1;
+          const reviewer = await User.findById(feedbacks[idx].userId);
+          reviewers.push(reviewer);
+        }
+      }
+      const results = [];
+      for (const result of Object.entries(resultsDict)) {
+        results.push({ name: result[0], value: result[1] });
+      }
 
       return h.view("proposals/review", {
         user: request.auth.credentials.user,
@@ -33,6 +62,8 @@ const register = function (server, options) {
         baseUrl: Config.get("/baseUrl"),
         proposal,
         feedback,
+        reviewers,
+        results,
         reviewedDateString: feedback ? feedback.createdAt.toString() : null,
         isReviewed: feedback ? true : false,
       });
@@ -83,12 +114,8 @@ const register = function (server, options) {
         let start;
         let end;
         if (request.query["uploadedAt"].includes(":")) {
-          start = new Date(
-            request.query["uploadedAt"].split(":")[0]
-          ).toISOString();
-          end = new Date(
-            request.query["uploadedAt"].split(":")[1]
-          ).toISOString();
+          start = new Date(request.query["uploadedAt"].split(":")[0]).toISOString();
+          end = new Date(request.query["uploadedAt"].split(":")[1]).toISOString();
         } else {
           const date = new Date(request.query["uploadedAt"]);
           start = date.toISOString();
@@ -131,14 +158,16 @@ const register = function (server, options) {
       };
 
       let reviewers;
-      if (user.roles.reviewer) { //get proposals that are assigned to the reviwer        
-        request.query.reviewerIds = user._id.toString(); 
+      if (user.roles.reviewer) {
+        //get proposals that are assigned to the reviwer
+        request.query.reviewerIds = user._id.toString();
       }
 
-      if (user.roles.chair) { //get proposals with feasibility status of approved when user is chair        
-        request.query.feasibilityStatus = 'Feasibility Checked'; 
+      if (user.roles.chair) {
+        //get proposals with feasibility status of approved when user is chair
+        request.query.feasibilityStatus = "Feasibility Checked";
         //get list of all available reviewers to be assigned a proposal
-        reviewers = await User.find({roles : {reviewer: true}});
+        reviewers = await User.find({ roles: { reviewer: true } });
       }
 
       const result = await Proposal.pagedLookup(
@@ -149,8 +178,7 @@ const register = function (server, options) {
         Proposal.lookups
       );
 
-
-      //attach list of full reviewers object to proposals 
+      //attach list of full reviewers object to proposals
       for (const proposal of result.data) {
         proposal.assignedReviewers = [];
         for (const id of proposal.reviewerIds) {
@@ -161,11 +189,22 @@ const register = function (server, options) {
 
       //logic for hasfeeback need to change here
       for (const proposal of result.data) {
-        const feedbackCnt = await Feedback.count({
+        const feedbacks = await Feedback.find({
           proposalId: proposal._id.toString(),
-          userId: user._id.toString(),
         });
-        proposal.hasFeedback = feedbackCnt > 0;
+
+        if (user.roles.chair) {
+          proposal.hasFeedback = feedbacks.length === proposal.reviewerIds.length;
+        } else if (user.roles.reviewer) {
+          for (const feedback of feedbacks) {
+            if (feedback.userId.toString() === user._id.toString()) {
+              proposal.hasFeedback = true;
+              break;
+            }
+          }
+        } else {
+          proposal.hasFeedback = false;
+        }
       }
 
       return h.view("proposals/submissions-list", {
@@ -181,7 +220,7 @@ const register = function (server, options) {
         totalNumPages: result.pages.total,
         total: result.items.total,
         notDatatableView: true,
-        reviewers        
+        reviewers,
       });
     },
   });
