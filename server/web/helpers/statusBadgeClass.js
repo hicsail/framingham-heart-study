a9b2725947e342de12e3aed3fc6464b3b35be925
wenhwang97 <wenhwang@bu.edu>
2023-03-29 10:45:05 -0400

    Revert "feat: add badge for final review"
    This reverts commit 26246fd6169dc9ddd2ab8ef4e484746594c7ec17.

--- server/web/helpers/statusBadgeClass.js
+++ server/web/helpers/statusBadgeClass.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
 module.exports = (status, prefix) => {
-  if (status === "Revise Requested" || status === "Reject") {
-    return prefix + "-danger";
-  } else if (status === "Feasibility Checked" || status === "Approve") {
-    return prefix + "-success";
-  } else if (status === "Revise") {
-    return prefix + "-warning";
-  }
-  return prefix + "-info";
+
+  	if (status === 'Revise Requested'){
+  		return prefix + '-danger';
+  	}
+  	else if (status === 'Feasibility Checked') {
+  		return prefix + '-success';
+  	}
+  	return prefix + '-info';
 };
