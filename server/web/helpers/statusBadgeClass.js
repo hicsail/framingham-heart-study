bf02e053745e6d14b236dbad342c2db7e0aa25f2
wenhwang97 <wenhwang@bu.edu>
2023-03-29 15:47:37 -0400

    feat: add badge for final review

--- server/web/helpers/statusBadgeClass.js
+++ server/web/helpers/statusBadgeClass.js
@@ -1,10 +1,12 @@
 "use strict";
 
 module.exports = (status, prefix) => {
-  if (status === "Revise Requested") {
+  if (status === "Revise Requested" || status === "Reject") {
     return prefix + "-danger";
-  } else if (status === "Feasibility Checked") {
+  } else if (status === "Feasibility Checked" || status === "Approve") {
     return prefix + "-success";
+  } else if (status === "Revise") {
+    return prefix + "-warning";
   }
   return prefix + "-info";
 };
