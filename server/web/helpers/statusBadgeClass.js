8e6bdb8560304d1bb756acf6c4ab55624e08c35c
wenhwang97 <wenhwang@bu.edu>
2023-03-31 14:57:45 -0400

    refactor: reformat code

--- server/web/helpers/statusBadgeClass.js
+++ server/web/helpers/statusBadgeClass.js
@@ -1,12 +1,10 @@
-'use strict';
+"use strict";
 
 module.exports = (status, prefix) => {
-
-  	if (status === 'Revise Requested'){
-  		return prefix + '-danger';
-  	}
-  	else if (status === 'Feasibility Checked') {
-  		return prefix + '-success';
-  	}
-  	return prefix + '-info';
+  if (status === "Revise Requested") {
+    return prefix + "-danger";
+  } else if (status === "Feasibility Checked") {
+    return prefix + "-success";
+  }
+  return prefix + "-info";
 };
